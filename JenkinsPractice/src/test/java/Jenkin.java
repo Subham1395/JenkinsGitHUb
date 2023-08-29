2204e1cceebf653124b77199d10ac38286576fb7
Subham1395 <dassubham1395@gmail.com>
2023-08-29 21:11:53 +0530

    1st commit

---
+++ JenkinsPractice/src/test/java/Jenkin.java
@@ -0,0 +1,12 @@
+import org.testng.annotations.Test;
+
+public class Jenkin {
+@Test
+	public void Demo() {
+		// TODO Auto-generated method stub
+		
+		System.out.println("hello jenkins");
+
+	}
+
+}
